b7b2137592a9164ed17206cf2fcc7e84cae4bead
Nam Nguyen <ewardsnguyen@gmail.com>
2017-12-03 20:27:50 +0700

    Modified: cartview

--- cart.php
+++ cart.php
@@ -112,8 +112,8 @@
     ?>  
     <div class="col-md-4">  
          <form method="post" action="cart.php?action=add&id=<?php echo $row["id"]; ?>">  
-              <div style="border:1px solid #333; background-color:#f1f1f1; border-radius:5px; padding:16px;" align="center">  
-                   <img src="<?php echo $row["imgurl"]; ?>" class="img-responsive" style = "width: 120px; height: 150px" /><br />  
+              <div style="background-color:#f1f1f1; border-radius:15px; padding:10px;" align="center">  
+                   <img src="<?php echo $row["imgurl"]; ?>" class="img-responsive"/><br />  
                    <h4 class="text-info"><?php echo $row["name"]; ?></h4>  
                    <h4 class="text-danger">$ <?php echo $row["price"]; ?></h4>  
                    <input type="text" name="quantity" class="form-control" value="1" />  
@@ -121,6 +121,7 @@
                    <input type="hidden" name="hidden_price" value="<?php echo $row["price"]; ?>" />  
                    <input type="submit" name="add_to_cart" style="margin-top:5px;" class="btn btn-success" value="Add to Cart" />  
               </div>  
+              <p></p>
          </form>  
     </div>  
     <?php  
