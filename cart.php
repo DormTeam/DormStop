5a9df93e223c68d8ad24b0d0d8caee28d2d00888
Nam Nguyen <ewardsnguyen@gmail.com>
2017-12-03 20:40:05 +0700

    Modified: cartview vnd price

--- cart.php
+++ cart.php
@@ -102,6 +102,46 @@
     </nav>
 
     <div class="container">
+    <div style="clear:both"></div>  
+    <br />  
+    <h3>Thông tin đặt hàng</h3>  
+    <div class="table-responsive">  
+         <table class="table table-bordered">  
+              <tr>  
+                   <th width="40%">Tên sản phẩm</th>  
+                   <th width="10%">Số lượng</th>  
+                   <th width="20%">Giá</th>  
+                   <th width="15%">Tổng tiền</th>  
+                   <th width="5%">Chỉnh sửa</th>  
+              </tr>  
+              <?php   
+              if(!empty($_SESSION["shopping_cart"]))  
+              {  
+                   $total = 0;  
+                   foreach($_SESSION["shopping_cart"] as $keys => $values)  
+                   {  
+              ?>  
+              <tr>  
+                   <td><?php echo $values["item_name"]; ?></td>  
+                   <td><?php echo $values["item_quantity"]; ?></td>  
+                   <td><?php echo $values["item_price"]; ?>Đ</td>  
+                   <td><?php echo number_format($values["item_quantity"] * $values["item_price"]); ?>Đ</td>  
+                   <td><a href="cart.php?action=delete&id=<?php echo $values["item_id"]; ?>"><span class="text-danger">Remove</span></a></td>  
+              </tr>  
+              <?php  
+                        $total = $total + ($values["item_quantity"] * $values["item_price"]);  
+                   }  
+              ?>  
+              <tr>  
+                   <td colspan="3" align="right">Total</td>  
+                   <td align="right"><?php echo number_format($total); ?>Đ</td>  
+                   <td></td>  
+              </tr>  
+              <?php  
+              }  
+              ?>  
+         </table>  
+        </div>
 
 	<?php  
         $query = "SELECT * FROM product ORDER BY id ASC";  
@@ -115,7 +155,7 @@
               <div style="background-color:#f1f1f1; border-radius:15px; padding:10px;" align="center">  
                    <img src="<?php echo $row["imgurl"]; ?>" class="img-responsive"/><br />  
                    <h4 class="text-info"><?php echo $row["name"]; ?></h4>  
-                   <h4 class="text-danger">$ <?php echo $row["price"]; ?></h4>  
+                   <h4 class="text-danger"><?php echo $row["price"]; ?>Đ</h4>  
                    <input type="text" name="quantity" class="form-control" value="1" />  
                    <input type="hidden" name="hidden_name" value="<?php echo $row["name"]; ?>" />  
                    <input type="hidden" name="hidden_price" value="<?php echo $row["price"]; ?>" />  
@@ -127,47 +167,7 @@
     <?php  
          }  
     }  
-    ?>  
-    <div style="clear:both"></div>  
-    <br />  
-    <h3>Order Details</h3>  
-    <div class="table-responsive">  
-         <table class="table table-bordered">  
-              <tr>  
-                   <th width="40%">Item Name</th>  
-                   <th width="10%">Quantity</th>  
-                   <th width="20%">Price</th>  
-                   <th width="15%">Total</th>  
-                   <th width="5%">Action</th>  
-              </tr>  
-              <?php   
-              if(!empty($_SESSION["shopping_cart"]))  
-              {  
-                   $total = 0;  
-                   foreach($_SESSION["shopping_cart"] as $keys => $values)  
-                   {  
-              ?>  
-              <tr>  
-                   <td><?php echo $values["item_name"]; ?></td>  
-                   <td><?php echo $values["item_quantity"]; ?></td>  
-                   <td>$ <?php echo $values["item_price"]; ?></td>  
-                   <td>$ <?php echo number_format($values["item_quantity"] * $values["item_price"], 2); ?></td>  
-                   <td><a href="cart.php?action=delete&id=<?php echo $values["item_id"]; ?>"><span class="text-danger">Remove</span></a></td>  
-              </tr>  
-              <?php  
-                        $total = $total + ($values["item_quantity"] * $values["item_price"]);  
-                   }  
-              ?>  
-              <tr>  
-                   <td colspan="3" align="right">Total</td>  
-                   <td align="right">$ <?php echo number_format($total, 2); ?></td>  
-                   <td></td>  
-              </tr>  
-              <?php  
-              }  
-              ?>  
-         </table>  
-        </div>  
+    ?>    
     </div>
 
 
